8195c36ca13a59b9a4eb9695acc3106f7b5ce1e9
itsmeganeshadhikari@gmail.com <itsmeganeshadhikari@gmail.com>
2024-01-08 21:21:24 +0545

    Default Path

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ const config: {
   // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
   // like '/berry-material-react/react/default'
   basename: "",
-  defaultPath: "/",
+  defaultPath: "/landing",
   fontFamily: `'Roboto', sans-serif`,
   borderRadius: 12,
   outlinedFilled: true,
