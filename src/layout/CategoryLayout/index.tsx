9c2daea89f5d8d4f05f2725414dcee3f13ced685
itsmeganeshadhikari@gmail.com <itsmeganeshadhikari@gmail.com>
2024-02-15 18:35:49 +0545

    Changes in category layout

--- src/layout/CategoryLayout/index.tsx
+++ src/layout/CategoryLayout/index.tsx
@@ -1,144 +1,39 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
-// material-ui
-import { styled, useTheme, Theme } from "@mui/material/styles";
-import {
-  Box,
-  Container,
-  CssBaseline,
-  Toolbar,
-  useMediaQuery,
-} from "@mui/material";
-
 // project imports
-import Breadcrumbs from "../../ui-component/extended/Breadcrumbs";
-import Header from "./Header";
-import navigation from "../../menu-items";
-import { drawerWidth } from "../../store/constant";
-import { SET_MENU } from "../../store/actions";
-import { DefaultRootStateProps } from "../../types";
-
-// assets
-import { IconChevronRight } from "@tabler/icons-react";
 import AppBar from "../../ui-component/extended/AppBar";
+import Footer from "../../views/pages/landing/Footer";
+import { styled } from "@mui/material";
+// ==============================|| MINIMAL LAYOUT ||============================== //
 
-interface MainStyleProps {
-  theme: Theme;
-  open: boolean;
-}
+const HeaderWrapper = styled("div")(({ theme }) => ({
+  overflowX: "hidden",
+  overflowY: "clip",
+  [theme.breakpoints.down("md")]: {
+    paddingTop: 20,
+  },
+}));
 
-// styles
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-  ({ theme, open }: MainStyleProps) => ({
-    ...theme.typography.mainContent,
-    ...(!open && {
-      borderBottomLeftRadius: 0,
-      borderBottomRightRadius: 0,
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      [theme.breakpoints.up("md")]: {
-        marginLeft: -(drawerWidth - 20),
-        width: `calc(100% - ${drawerWidth}px)`,
-      },
-      [theme.breakpoints.down("md")]: {
-        marginLeft: "20px",
-        width: `calc(100% - ${drawerWidth}px)`,
-        padding: "16px",
-      },
-      [theme.breakpoints.down("sm")]: {
-        marginLeft: "10px",
-        width: `calc(100% - ${drawerWidth}px)`,
-        padding: "16px",
-        marginRight: "10px",
-      },
-    }),
-    ...(open && {
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-      borderBottomLeftRadius: 0,
-      borderBottomRightRadius: 0,
-      width: `calc(100% - ${drawerWidth}px)`,
-      [theme.breakpoints.down("md")]: {
-        marginLeft: "20px",
-      },
-      [theme.breakpoints.down("sm")]: {
-        marginLeft: "10px",
-      },
-    }),
-  })
+const SecondWrapper = styled("div")(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    paddingTop: 15,
+  },
+  [theme.breakpoints.down("sm")]: {
+    paddingTop: 100,
+    paddingBottom: 100,
+  },
+}));
+
+const CategoryLayout = () => (
+  <>
+    <HeaderWrapper id="home">
+      <AppBar />
+    </HeaderWrapper>
+    <SecondWrapper>
+      <Outlet />
+    </SecondWrapper>
+    <Footer />
+  </>
 );
-
-// ==============================|| MAIN LAYOUT ||============================== //
-
-const CategoryLayout = () => {
-  const theme = useTheme();
-  const matchDownMd = useMediaQuery(theme.breakpoints.down("lg"));
-
-  // Handle left drawer
-  const leftDrawerOpened = useSelector(
-    (state: DefaultRootStateProps) => state.customization.opened
-  );
-  const dispatch = useDispatch();
-  const handleLeftDrawerToggle = () => {
-    dispatch({ type: SET_MENU, opened: !leftDrawerOpened });
-  };
-
-  React.useEffect(() => {
-    dispatch({ type: SET_MENU, opened: !matchDownMd });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [matchDownMd]);
-
-  return (
-    <Container>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        {/* header */}
-        <AppBar
-          enableColorOnDark
-          position="fixed"
-          color="inherit"
-          elevation={0}
-          sx={{
-            bgcolor: theme.palette.background.default,
-            transition: leftDrawerOpened
-              ? theme.transitions.create("width")
-              : "none",
-          }}
-        >
-          <Toolbar>
-            <Header handleLeftDrawerToggle={handleLeftDrawerToggle} />
-          </Toolbar>
-        </AppBar>
-
-        {/* drawer */}
-        {/* <CategorySidebar
-          drawerOpen={leftDrawerOpened}
-          drawerToggle={handleLeftDrawerToggle}
-        /> */}
-
-        {/* main content */}
-        <Main theme={theme} open={leftDrawerOpened}>
-          {/* breadcrumb */}
-          <Breadcrumbs
-            separator={IconChevronRight}
-            navigation={navigation}
-            icon
-            title
-            rightAlign
-          />
-          <Outlet />
-        </Main>
-        {/* <Customization /> */}
-      </Box>
-    </Container>
-  );
-};
 
 export default CategoryLayout;
